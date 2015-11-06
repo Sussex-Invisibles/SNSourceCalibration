Fixed errors with interpolation due to noise, any time it cant do a accurate width calculation return nan

calc_utils.py
@@ -37,7 +37,11 @@ def readTRCFiles(file_directory, correct_offset=True):
     y = np.zeros( (count, len(xRaw)) )
     for i, ent in enumerate(yRaw):
         if correct_offset == True:
-            y[i, :] = ent  - np.mean(ent[0:20])
+            try:
+                y[i, :] = ent  - np.mean(ent[0:20])
+            except ValueError:
+                print ent
+                print "Background Mean: "+str(np.mean(ent[0:20]))
         else:
             y[i, :] = ent  
     return x,y
@@ -88,23 +92,19 @@ def rms(alist):
 
 def interpolate_threshold(x, y, thresh, rise=True, start=0):
     """Calculate the threshold crossing using a linear interpolation"""
-    if len(x)<5 or len(y)<5:
+    if len(x)<10 or len(y)<10:
         return np.nan 
     if rise == True:
-        index_high = np.where( y > thresh )[0][start]
+        try:
+            index_high = np.where( y > thresh )[0][start]
+        except IndexError:
+            return np.nan
     else:
-        index_high = np.where( y < thresh )[0][start]
+        try:
+            index_high = np.where( y < thresh )[0][start]
+        except  IndexError:
+            return np.nan
     index_low = index_high - 1
-    """if x[index_high]<x[index_low]:
-        print "PRINTING"
-        for i in range(len(x)):
-            print str(x[i])+" "+str(y[i])
-    print "index_low: "+str(index_low)
-    print "index_high: "+str(index_high)
-    print "y_low: "+str(y[index_low])+" y_high: "+str(y[index_high])
-    print "x_low: "+str(x[index_low])+" x_high: "+str(x[index_high])
-    print "dydx: "+str((y[index_high] - y[index_low])/(x[index_high]-x[index_low]))
-    """
     dydx = (y[index_high] - y[index_low])/(x[index_high]-x[index_low])
     time = x[index_low] + (thresh - y[index_low]) / dydx
     #print "x0 = %1.1f\t(thresh - y0) = %1.1f\tdydx = %1.3f\ttime = %1.1f\tdx = %1.1f" % (x[index_low], (thresh - y[index_low]), dydx, time, (x[index_high] - x[index_low])) 

drawSingleTrace.py
@@ -1,10 +1,11 @@
 import root_utils
 import calc_utils
 import os
+import sys
 import ROOT
 import matplotlib.pyplot as plt
 import numpy as np
-x,y = calc_utils.readTRCFiles(os.path.join("dataset","4000"),correct_offset=True)
+x,y = calc_utils.readTRCFiles(os.path.join(sys.argv[1],"dataset",sys.argv[2]),correct_offset=False)
 ymean = np.mean(y,0)
 for i in  range(len(x)):
     print str(x[i])+"    "+str(y[0][i])

root_utils.py
@@ -91,7 +91,7 @@ def plot_fall(x, y, name, scale = 1e9):
             hist.Fill((low - high)*scale)
     return hist, fall, fallErr
 
-def plot_width(x, y, name, scale = 1):
+def plot_width(x, y, name, scale = 1e8):
     """Calc and plot FWHM of pulses"""
     width, widthErr = calc.calcWidth(x,y)
     print width, widthErr
@@ -105,8 +105,8 @@ def plot_width(x, y, name, scale = 1):
             m = max(y[i,:])
             m_index = np.where(y[i,:] == m)[0][0]
             thresh = m*0.5
-            first = calc.interpolate_threshold(x[:m_index+1], y[i,:m_index+1], thresh, rise=True)
-            second = calc.interpolate_threshold(x[m_index-1:], y[i,m_index-1:], thresh, rise=False)
+            first = calc.interpolate_threshold(x[:m_index], y[i,:m_index], thresh, rise=True)
+            second = calc.interpolate_threshold(x[m_index:], y[i,m_index:], thresh, rise=False)
             hist.Fill((second - first)*scale)
     else:
         for i in range(len(y[:,0])-1):
@@ -114,8 +114,9 @@ def plot_width(x, y, name, scale = 1):
             m_index = np.where(y[i,:] == m)[0][0]
             thresh = m*0.5
             #print "Negative Pulse Threshold is: "+str(thresh)
-            first = calc.interpolate_threshold(x[:m_index+1], y[i,:m_index+1], thresh, rise=False)
-            second = calc.interpolate_threshold(x[m_index-1:], y[i,m_index-1:], thresh, rise=True)
+            first = calc.interpolate_threshold(x[:m_index], y[i,:m_index], thresh, rise=False)
+            second = calc.interpolate_threshold(x[m_index:], y[i,m_index:], thresh, rise=True)
+            print "Width: "+str(second-first)
             hist.Fill((second - first)*scale)
     return hist, width, widthErr
 