4df1a1b3fec83d55dd3dcd726563e37a17e14804
Mark Stringer <ms711@sussex.ac.uk>
2015-12-01 13:54:39 -0500

    Updated scale plotter

--- plotTextFilesScale.py
+++ plotTextFilesScale.py
@@ -63,11 +63,11 @@ for iteration in range(len(fileArray)):
     print os.path.basename(fileArray[iteration])
     print totYVals[iteration]
     plt.figure(0)
-    plt.errorbar(xVals,np.multiply(totYVals[iteration],-1*scaleArray[iteration]),yerr=totYErr[iteration],label=str((labelArray[iteration])))
+    plt.errorbar(totXVals[iteration],np.multiply(totYVals[iteration],-1*scaleArray[iteration]),yerr=totYErr[iteration],label=str((labelArray[iteration])))
     negyVals = np.multiply(totYVals[iteration],-1.0)
     #print np.divide(yErrs,yVals)
     plt.figure(1)
-    plt.plot(xVals,np.divide(totYErr[iteration],negyVals),label="Error over area: "+str(labelArray[iteration]))
+    plt.plot(totXVals[iteration],np.divide(totYErr[iteration],negyVals),label="Error over area: "+str(labelArray[iteration]))
     if doFit[iteration]:
         plt.figure(0)
         fitWeights = []
@@ -77,22 +77,22 @@ for iteration in range(len(fileArray)):
         upIndex = 0
         
         for i in range(len(xVals)):
-            if xVals[i] > lowFitLimits[fitCounter]:
+            if totXVals[iteration][i] > lowFitLimits[fitCounter]:
                 lowIndex = i 
                 break
         
         for i in range(len(xVals)-1,0,-1):
-            if xVals[i] < upFitLimits[fitCounter]:
+            if totXVals[iteration][i] < upFitLimits[fitCounter]:
                 upIndex = i+1
                 break
        
         fitCounter += 1
-        fitResults = np.polyfit(xVals[lowIndex:upIndex],np.multiply(yVals[lowIndex:upIndex],-1.0),2,w=fitWeights[lowIndex:upIndex],full=True)
+        fitResults = np.polyfit(totXVals[iteration][lowIndex:upIndex],np.multiply(yVals[lowIndex:upIndex],-1.0),2,w=fitWeights[lowIndex:upIndex],full=True)
         fitValues = fitResults[0]
         chi_squared = fitResults[1]
         poly = np.poly1d(fitValues)
-        plt.plot(xVals[lowIndex:upIndex],poly(xVals[lowIndex:upIndex]),label="Fit to: "+(labelArray[iteration]))
-        reduced_chi_squared = chi_squared/(len(xVals[lowIndex:upIndex])-len(fitValues))
+        plt.plot(totXVals[iteration][lowIndex:upIndex],poly(totXVals[iteration][lowIndex:upIndex]),label="Fit to: "+(labelArray[iteration]))
+        reduced_chi_squared = chi_squared/(len(totXVals[iteration][lowIndex:upIndex])-len(fitValues))
         print "Parameters for fit to: "+str(os.path.basename(fileArray[iteration]))+"    "+str(fitValues)
         print "Number of Degrees of Freedom is: "+str(len(xVals[lowIndex:upIndex])-len(fitValues))
         print "chi squared is: "+str(chi_squared)
